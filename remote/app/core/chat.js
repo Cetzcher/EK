99fe2338b46a6ab4097682545e2411de87a40851
Pierre <pierre.rieger.rcc@gmali.com>
2017-10-20 10:29:07 +0200

    inital

---
+++ remote/app/core/chat.js
@@ -0,0 +1,115 @@
+function Chat(){
+	this.users = [];
+	this.msgs = [];
+}
+
+// add a user to the chat
+Chat.prototype.add_user = function(user){
+	this.users.push(user);
+};
+
+// send a msg to the chat, true if msg has been sent, false otherwise.
+Chat.prototype.send = function(user, txt){
+	if(this.is_member(user)){
+		this.msgs.push({msg: txt, read_by: [], time: "LOCAL TIME", sender: user});
+		return true; // sent
+	}
+	return false;
+};
+
+// get the entire chat in the form of: [{msg: theMsg, sent_by: theSender, time: theSendTime}, ...]
+Chat.prototype.get = function(){
+	var m = [];
+	for(var i = 0; i < this.msgs.length; i++){
+		var item = this.msgs[i];
+		m.push({msg: item.msg, sent_by: item.sender, time: item.time});
+	}
+	return m; // all msgs without user data and time
+};
+
+// get only the unread chat of a user.
+// return an object array format: [{msg: theMsg, sent_by: theSender, time: theSendTime}, ...]
+// if 'by' is not a memebr of the chat, return false.
+Chat.prototype.get_unread = function(by){
+	if(!this.is_member(by))
+		return false;
+	var m = [];
+	for(var i = 0; i < this.msgs.length; i++){
+		var item = this.msgs[i];
+		var readers = item.read_by;
+		if(readers.indexOf(by) === -1)
+		{
+			// item is not read by the user
+			m.push({msg: item.msg, sent_by: item.sender, time: item.time});
+			// add the user to the read list.
+			this.msgs[i].read_by.push(by);
+		}
+	}
+	return m;
+};
+
+// check if the user is a member of this chat
+Chat.prototype.is_member = function(user){
+	return this.users.indexOf(user)  > -1; 
+};
+
+//===========================================
+//----------------CHAT HANDLER---------------
+//===========================================
+
+// initalize the chat handler
+function ChatHandler () {
+	this.chats = {};
+	this.last_id = 0;
+}
+
+// create a chat
+ChatHandler.prototype.create_chat = function(){
+	var id = this.last_id;
+	this.last_id++;
+	this.chats[id] = new Chat();
+	return id;
+};
+
+// get the chat with the id 'id'
+ChatHandler.prototype.get = function(id){
+	return this.chats[id];
+};
+
+// reutrn all chats in which 'user' is a member as an array of ids
+ChatHandler.prototype.get_chats_with_user = function(user){
+	var keys = Object.keys(this.chats);
+	var m = [];
+	for(var i = 0; i < keys.length; i++){
+		var chat = this.chats[keys[i]];
+		if(chat.is_member(user))
+			m.push(key);
+	}
+	return m;
+};
+
+// get a list of ids of shared chats of [users]
+ChatHandler.prototype.common_chat = function(users){
+	var shared = [];
+	var keys = Object.keys(this.chats);
+	// check each chat.
+	for(var i = 0; i < keys.length; i++){
+		var key = keys[i];
+		var val = this.chats[key]; // the chat
+		var shares = true;
+		// check if each user is a member of the chat
+		for(var member = 0; member < users.length; member++){
+			if(val.is_member(users[member])){
+				shares = false;
+				break;
+			}
+		}
+		if(shares)
+			shared.push(key);
+	}
+	return shared;
+};
+
+
+module.exports = new ChatHandler();
+
---
+++ remote/app/routes/index.js
@@ -0,0 +1,169 @@
+var express = require('express');
+var router = express.Router();
+var auth = require("../core/authenticate");
+var user = require("../core/user");
+var chatHandler = require("../core/chat");
+
+function get_token(request){
+    return request.body.token; // change this later
+}
+
+router.post('/authenticate', function (req, res) {
+	// authenticate the user, give him a token or reply with an error.
+    // on success: {success: true, token: token}
+    // on error: {success: false, error: err_msg}
+    auth.auth(req.body, function(result){
+        res.send(result);
+    });
+});
+
+// get all users.
+router.get('/users', function (req, res) {
+    user.get_all(function(names){
+        res.json({user_names: names});
+    });
+});
+
+// register the user, expects a username, password and email, the password must be encrypted already.
+// on success return a JSON object {success: true, error: ""}
+ // on error return a JSON object {success: false, error: err_msg}
+router.post('/register', function(req, res){
+
+    user.create_user(req.body, function(result){
+            return res.json(result);
+        });
+});
+
+router.delete("/delete/:token", function(req, res)
+{
+	// delete the user.
+});
+
+var current_token = null;
+// require authetication for all /auth/*
+// authenticate the user, if the user is not authenticated, respond with forbidden.
+router.all("/auth/*", function(req, res, next){
+    current_token = get_token(req);
+    console.log("authenticating with: " + current_token);
+    if(auth.is_auth(current_token))
+    {
+        // token is valid, proceed
+        next();
+        return;
+    }
+    // if token is invaild return forbidden
+    res.status(403);
+    res.send("Forbidden");
+
+});
+
+// log out the user, destroy token.
+router.post("/auth/logout", function(req, res){
+    auth.remove(current_token);
+    res.send("logged out");
+});
+
+
+router.post('/auth/test', function (req, res) {
+  res.send('Birds home page');
+});
+
+
+// list chats of authenticated user.
+router.post("/auth/list_chats", function(req, res) {
+    // get user
+    // get user's chats
+    // return list of chat_ids along with the name of the other user for displaying
+});
+
+// on success: 
+//      when new chat created: {success: true, id: [chat_id], info: "chat_created"}
+//      when chat existed already: {success: true, id: [chat_id], info: "chat already existed"}
+//      when group chats are implemented, users could shate multiple chats, so return a list of ids.
+// on failure: {success: false, error: "could not fetch other user"}
+router.post("/auth/start_chat/:name", function(req, res){
+    // create a chat connection between two users
+    // the first user is obtained from the token, the other
+    // user is obtained by the url_param
+    var self = auth.is_auth(current_token); // get the username of the sender
+    user.exists(req.params, function(result){
+        if(result.success)
+        {
+            // start chat with user, send success
+            var other = req.params.name;
+            // check if the chat exists:
+            var shared = chatHandler.common_chat([self, other]);
+            // if there is shared chat, return the chat ID.
+            
+            console.log("shared ids for: " + self + ":" + other);
+            console.log(shared);
+
+            if(shared.length > 0){
+                console.log("has shared chat");
+                return res.json({success: true, id: shared, info: "chat already existed"});
+            }else{
+                console.log("creating shared chat");
+                // if not create the chat, return chat_id
+                var chat_id = chatHandler.create_chat();
+                var chat = chatHandler.get(chat_id);
+                chat.add_user(self);
+                chat.add_user(other);
+                return res.json({success: true, id: [chat_id], info: "chat_created"});
+            }
+
+            return res.send("chat started between: " + self + " other: " + other);
+        }
+        return res.json({success: false, error: "could not fetch other user"});
+    });
+});
+
+// read from chat, if get_all is true then get the entire chat
+// otherwise get only the unread chat
+function read(req, res, get_all){
+    var self = auth.is_auth(current_token);
+    var chat_id = req.params.chat_id;
+    var chat = chatHandler.get(chat_id);
+    if(chat)
+    {
+        // read from chat.
+        var g = "";
+        if(get_all)
+            g = chat.get();
+        else
+            g = chat.get_unread(self);
+
+        return res.json({success: true, chat_msgs: g, id: chat_id});
+    }
+    return res.json({success: false, error: "chat id does not exist"});
+}
+
+// read from chat
+// return unsuccessful if the chat id does not exist
+router.post("/auth/read_all/:chat_id", function(req, res){
+    return read(req, res, true);
+});
+
+
+router.post("/auth/read/:chat_id", function(req, res){
+    return read(req, res, false);
+});
+
+
+
+// write to chat
+router.post("/auth/send/:chat_id", function(req, res){
+    // write to chat with id
+    var self = auth.is_auth(current_token);
+    var chat_id = req.params.chat_id;
+    var chat = chatHandler.get(chat_id);
+    if(chat)
+    {
+        // read from chat.
+        return res.json({success: chat.send(self, req.body.msg), wrote: req.body.msg, id: chat_id});
+    }
+    return res.json({success: false, error: "chat id does not exist"});
+});
+
+
+
+module.exports = router;
